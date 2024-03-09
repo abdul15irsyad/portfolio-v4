ac7304b1e371ba456485de0ffecdf59cd47521d9
Irsyad Abdul <abdulirsyad15@gmail.com>
2024-03-09 12:07:29 +0700

    paginate portfolios

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -2,79 +2,100 @@
 
 import Empty from '@/components/empty';
 import FormSelect from '@/components/form-select';
-import { portfolios } from '@/data/portfolios.data';
+import { portfolios as allPortfolios } from '@/data/portfolios.data';
 import React, { useEffect, useState } from 'react';
-import AOS from 'aos';
-import { aosInitConfig } from '@/configs/aos.config';
 import PortfolioItem2 from '@/components/portfolio-item-2';
 import {
   portfolioCategories,
   portfolioYears,
 } from '@/data/portfolio-category.data';
+import { paginatedArray } from '@/utils/array.util';
+import Pagination from '@/components/pagination';
+import { Portfolio } from '@/types/portfolio.type';
 
 export default () => {
+  const [page, setPage] = useState<number>(1);
   const [year, setYear] = useState<string | number>('all');
   const [type, setType] = useState<string>('all');
-  const [filteredPortfolios, setFilteredPortofolio] = useState(portfolios);
+  const limit = 6;
+  const [portfolios, setPortofolios] = useState(
+    paginatedArray(allPortfolios, { page, limit }),
+  );
+  const [totalAllData, setTotalAllData] = useState<number>(
+    allPortfolios.length,
+  );
+  const filterPortfolios = (portfolios: Portfolio[]) =>
+    portfolios.filter((portfolio) => {
+      return (
+        (year !== 'all' ? portfolio.year === +year : true) &&
+        (type !== 'all' ? portfolio.type.toLowerCase() === type : true)
+      );
+    });
 
+  // on year or type change
   useEffect(() => {
-    AOS.init(aosInitConfig);
-  });
-
-  useEffect(() => {
-    setFilteredPortofolio(
-      portfolios.filter((portfolio) => {
-        return (
-          (year !== 'all' ? portfolio.year === +year : true) &&
-          (type !== 'all' ? portfolio.type.toLowerCase() === type : true)
-        );
-      }),
-    );
+    const filteredPortfolios = filterPortfolios(allPortfolios);
+    setPage(1);
+    setPortofolios(paginatedArray(filteredPortfolios, { page, limit }));
+    setTotalAllData(filteredPortfolios.length);
   }, [year, type]);
+
+  // on page change
+  useEffect(() => {
+    const filteredPortfolios = filterPortfolios(allPortfolios);
+    setPortofolios(paginatedArray(filteredPortfolios, { page, limit }));
+  }, [page]);
   return (
-    <>
-      <div className="portfolio section doodle-background">
-        <div className="container">
-          <div className="row header">
-            <div className="col">
-              <h1 className="title">Portfolio</h1>
-              <hr />
-              <div className="filters">
-                <div className="filter filter-year">
-                  <FormSelect
-                    options={portfolioYears}
-                    handleChange={(e) => setYear(e.target.value)}
-                  />
-                </div>
-                <div className="filter filter-type">
-                  <FormSelect
-                    options={portfolioCategories}
-                    handleChange={(e) => setType(e.target.value)}
-                  />
-                </div>
+    <div className="portfolio section doodle-background">
+      <div className="container">
+        <div className="row header">
+          <div className="col">
+            <h1 className="title">Portfolio</h1>
+            <hr />
+            <div className="filters">
+              <div className="filter filter-year">
+                <FormSelect
+                  options={portfolioYears}
+                  handleChange={(e) => setYear(e.target.value)}
+                />
               </div>
-              <p className="text-center text-secondary mt-3 mb-0">
-                Showing <strong>{filteredPortfolios.length}</strong> of{' '}
-                <strong>{portfolios.length}</strong> portfolios
-              </p>
+              <div className="filter filter-type">
+                <FormSelect
+                  options={portfolioCategories}
+                  handleChange={(e) => setType(e.target.value)}
+                />
+              </div>
             </div>
-          </div>
-          <div className="row porfolios">
-            {filteredPortfolios.length > 0 ? (
-              filteredPortfolios.map((portfolio, index) => (
-                <div
-                  key={index}
-                  className={`col-xl-4 col-md-6 col-12 gx-4 gy-md-4 gy-3`}
-                >
-                  <PortfolioItem2 {...portfolio} />
-                </div>
-              ))
-            ) : (
-              <Empty />
-            )}
+            <p className="text-center text-secondary mt-3 mb-0">
+              Showing <strong>{portfolios.length}</strong> of{' '}
+              <strong>{totalAllData}</strong> portfolios
+            </p>
           </div>
         </div>
+        <div className="row porfolios mb-lg-5 mb-3">
+          {portfolios.length > 0 ? (
+            portfolios.map((portfolio, index) => (
+              <div
+                key={index}
+                className={`col-xl-4 col-md-6 col-12 gx-4 gy-md-4 gy-3`}
+              >
+                <PortfolioItem2 {...portfolio} />
+              </div>
+            ))
+          ) : (
+            <Empty />
+          )}
+        </div>
+        <Pagination
+          position="center"
+          activePage={page}
+          setPage={({ page }) => {
+            setPage(page);
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+          }}
+          totalPage={Math.ceil(totalAllData / limit)}
+        />
       </div>
-    </>
+    </div>
   );
 };
