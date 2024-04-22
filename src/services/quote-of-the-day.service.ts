a65f9f93d8f83cbbda7d069d95a5eec9da456f77
Irsyad Abdul <abdulirsyad15@gmail.com>
2024-04-22 17:53:03 +0700

    fix: update quote of the day caching

--- src/services/quote-of-the-day.service.ts
+++ src/services/quote-of-the-day.service.ts
@@ -1,9 +1,15 @@
 import { QuoteOfTheDay } from '@/types/quote-of-the-day.type';
+import { random } from '@/utils/array.util';
+import { randomInt } from '@/utils/number.util';
 
 export const getQuoteOfTheDay = async (): Promise<QuoteOfTheDay> => {
+  const tags = ['motivational', 'inspirational', 'famous quotes'];
   const res = await (
     await fetch(
-      'https://api.quotable.io/random?maxLength=200&tags="motivational|inspirational|famous quotes"',
+      `https://api.quotable.io/random?maxLength=${randomInt(
+        150,
+        200,
+      )}&tags=${random(tags)}`,
     )
   ).json();
   return res;
