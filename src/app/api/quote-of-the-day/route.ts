c5df2a87f86c828bddd7e8727fc32efc4650b3f2
Irsyad Abdul <abdulirsyad15@gmail.com>
2024-04-24 08:04:42 +0700

    feat: add loading to quote of the day

--- src/app/api/quote-of-the-day/route.ts
+++ src/app/api/quote-of-the-day/route.ts
@@ -1,19 +1,14 @@
 import { cache } from '@/redis/redis.util';
 import { getQuoteOfTheDay } from '@/services/quote-of-the-day.service';
-import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc';
-import timezone from 'dayjs/plugin/timezone';
 import { NextResponse } from 'next/server';
-dayjs.extend(utc);
-dayjs.extend(timezone);
 
-export async function GET() {
-  const date = dayjs().utc().format('YYYY-MM-DD');
-  console.log('date quote of the day', date);
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const date = searchParams.get('date');
   const quoteOfTheDay = await cache(
     `quoteOfTheDay:${date}`,
     () => getQuoteOfTheDay(),
-    5 * 60,
+    24 * 60 * 60,
   );
   return NextResponse.json({
     message: 'get quote of the day',
