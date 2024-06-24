ff6ee5aafa8aaa86daf84a04c282a5c417b02b25
Irsyad Abdul <abdulirsyad15@gmail.com>
2024-06-24 07:38:21 +0700

    data: update awards data

--- src/data/awards.data.ts
+++ src/data/awards.data.ts
@@ -6,7 +6,8 @@ export const awards: Award[] = [
     competition:
       'Telkomsel Tech Titans League Series 1 - Software Development 2020',
     certificate: {
-      href: 'https://drive.google.com/file/d/17_TdqmVD-srVeqIAB433OCuvW-4dDEdq/view?usp=sharing',
+      // href: 'https://drive.google.com/file/d/17_TdqmVD-srVeqIAB433OCuvW-4dDEdq/view?usp=sharing',
+      href: '/Certificate Telkomsel Tech Titans League - Series Software Development (Super Titans).pdf',
     },
   },
 ];
