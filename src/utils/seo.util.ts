11e4960be1a0a77d3cd836625fe7f83362fa5d8e
Irsyad Abdul Hamid Darussalam <90173897@bri.co.id>
2024-07-10 16:56:31 +0700

    fix: remove puppeteer

--- src/utils/seo.util.ts
+++ src/utils/seo.util.ts
@@ -1,31 +1,15 @@
 import { load } from 'cheerio';
-import puppeteer from 'puppeteer';
 
 export const extractSeoData = async (url: string) => {
   try {
-    const dynamicPageDomains: string[] = [];
-    const parsedUrl = new URL(url);
-    let $: any;
-    const isDynamicPage = dynamicPageDomains.includes(
-      parsedUrl.hostname.toLowerCase(),
-    );
-    if (isDynamicPage) {
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
-      await page.goto(url, { waitUntil: 'networkidle0' });
-      const html = await page.content();
-      $ = load(html);
-      await browser.close();
-    } else {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(
-          `error fetching url: ${url}, status code: ${response.status}`,
-        );
-      }
-      const html = await response.text();
-      $ = load(html);
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(
+        `error fetching url: ${url}, status code: ${response.status}`,
+      );
     }
+    const html = await response.text();
+    const $ = load(html);
 
     const title = $('title')?.text().trim();
     const desc = $('meta[property="og:description"]').attr('content')?.trim();
