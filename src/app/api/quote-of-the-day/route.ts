b316bb1891ae7f5b5befcca03454d86ae5ae4cd6
Irsyad Abdul <abdulirsyad15@gmail.com>
2024-09-13 18:06:09 +0700

    fix: fix quote of the day endpoints, add https agent

--- src/app/api/quote-of-the-day/route.ts
+++ src/app/api/quote-of-the-day/route.ts
@@ -2,20 +2,32 @@ import { NextResponse } from 'next/server';
 
 import { cache } from '@/redis/redis.util';
 import { getQuoteOfTheDay } from '@/services/quote-of-the-day.service';
+import { handleError } from '@/utils/error.util';
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const date = searchParams.get('date');
-  const quoteOfTheDay = await cache(
-    `quoteOfTheDay:${date}`,
-    () => getQuoteOfTheDay(),
-    24 * 60 * 60,
-  );
-  return NextResponse.json({
-    message: 'get quote of the day',
-    data: {
-      date,
-      quote: quoteOfTheDay,
-    },
-  });
+  try {
+    const { searchParams } = new URL(request.url);
+    const date = searchParams.get('date');
+    const quoteOfTheDay = await cache(
+      `quoteOfTheDay:${date}`,
+      () => getQuoteOfTheDay(),
+      24 * 60 * 60,
+    );
+
+    return NextResponse.json({
+      message: 'get quote of the day',
+      data: {
+        date,
+        quote: quoteOfTheDay,
+      },
+    });
+  } catch (error) {
+    handleError(error);
+    return NextResponse.json(
+      {
+        message: 'internal server error',
+      },
+      { status: 500 },
+    );
+  }
 }
