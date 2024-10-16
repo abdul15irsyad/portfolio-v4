bd4b13331669d5a0ac315f86adf71cbeea7a7538
Irsyad Abdul Hamid Darussalam <90173897@bri.co.id>
2024-10-16 22:51:53 +0700

    feat: add rabbitmq to skills

--- src/components/provider/analytics-provider.tsx
+++ src/components/provider/analytics-provider.tsx
@@ -11,24 +11,23 @@ interface Props {
 
 export const AnalyticsProvider = ({ children }: Props) => {
   const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
-  const DOMAIN = 'https://www.googletagmanager.com';
   const REPORT_DOMAIN = 'https://www.google-analytics.com';
 
   return (
     <>
       {children}
-      {ENV === 'production' && (
-        <>
-          <Script
-            id="google-analytics-js-cdn"
-            src={`${DOMAIN}/gtag/js?id=${GA_MEASUREMENT_ID}`}
-            strategy="afterInteractive"
-          />
-          <Script
-            id="google-analytics"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
+      {/* {ENV === 'production' && ( */}
+      <>
+        <Script
+          id="google-analytics-js-cdn"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
@@ -39,10 +38,10 @@ export const AnalyticsProvider = ({ children }: Props) => {
                  first_party_collection: true
               });
           `,
-            }}
-          />
-        </>
-      )}
+          }}
+        />
+      </>
+      {/* )} */}
     </>
   );
 };
