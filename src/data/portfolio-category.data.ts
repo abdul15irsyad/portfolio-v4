7b0c4e3e50ec15fdc86ed513882a82d07afdc0a7
Irsyad Abdul Hamid Darussalam <90173897@bri.co.id>
2025-08-22 00:37:33 +0700

    fix(portfolio): add missing year 2025 to portfolioYears data

--- src/data/portfolio-category.data.ts
+++ src/data/portfolio-category.data.ts
@@ -21,6 +21,7 @@ export const portfolioCategories = [
 
 export const portfolioYears = [
   { value: 'all', label: 'all-year', selected: true },
+  { value: '2025', label: '2025', selected: false },
   { value: '2024', label: '2024', selected: false },
   { value: '2023', label: '2023', selected: false },
   { value: '2022', label: '2022', selected: false },
