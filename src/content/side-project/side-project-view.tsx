bb72ffd0af0eee741e56e07f0e97366e556eb382
Irsyad Abdul <irsyad.darussalam@krom.id>
2026-06-05 17:03:42 +0700

    refactor(side-project): simplify filtering and state management

--- src/content/side-project/side-project-view.tsx
+++ src/content/side-project/side-project-view.tsx
@@ -2,18 +2,16 @@
 
 import Image from 'next/image';
 import { parseAsInteger, useQueryState } from 'nuqs';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import slugify from 'slugify';
 
 import { ClearButton } from '@/components/clear-button';
 import { Empty } from '@/components/empty/empty';
 import { Pagination } from '@/components/pagination/pagination';
 import { SectionTitle } from '@/components/section-title/section-title.component';
 import { SideProjectItem } from '@/content/side-project/side-project-item';
-import {
-  allStacks,
-  sideProjects as allSideProjects,
-} from '@/data/side-projects.data';
+import { sideProjects } from '@/data/side-projects.data';
 import { SideProject } from '@/types/side-project.type';
 import { paginatedArray } from '@/utils/array.util';
 import { capitalizeEachWord } from '@/utils/change-case';
@@ -22,64 +20,54 @@ export const SideProjectView = () => {
   const { t } = useTranslation();
   const [page, setPage] = useQueryState('page', parseAsInteger.withDefault(1));
   const limit = 6;
-  const [sideProjects, setSideProjects] = useState(
-    paginatedArray(allSideProjects, { page, limit }),
-  );
-  const [totalAllData, setTotalAllData] = useState<number>(
-    allSideProjects.length,
+  const [filteredSideProjects, setFilteredSideProjects] =
+    useState(sideProjects);
+  const paginatedSideProjects = useMemo(
+    () => paginatedArray(filteredSideProjects, { page, limit }),
+    [filteredSideProjects, page],
   );
   const [activeStacks, setActiveStacks] = useState<string[]>([]);
-  const [stacks, setStacks] = useState(
-    allStacks.map((stack) => ({
-      ...stack,
-      isActive: activeStacks?.includes(stack.label),
-    })),
+  const getSlugFromStackIcon = useCallback(
+    (icon: string) => slugify(icon.split('/').pop()!.split('.')[0]!),
+    [],
   );
-  useEffect(() => {
-    setStacks((stacks) =>
-      stacks.map((stack) =>
-        activeStacks?.includes(stack.slug)
-          ? { ...stack, isActive: true }
-          : { ...stack, isActive: false },
-      ),
-    );
-  }, [activeStacks]);
-  const filterSideProjects = useCallback(
-    (sideProjects: SideProject[]) => {
-      const activeStacks = stacks.filter(({ isActive }) => isActive);
-      return activeStacks.length > 0
-        ? sideProjects.filter((sideProject) =>
-            activeStacks.every((activeStack) =>
-              sideProject.stacks.find(({ icon }) => icon === activeStack.icon),
-            ),
-          )
-        : sideProjects;
-    },
-    [stacks],
+  const stacks = useMemo(
+    () =>
+      filteredSideProjects
+        .reduce((prev: SideProject['stacks'], curr) => {
+          return [
+            ...prev,
+            ...(curr.stacks?.filter(
+              (stack) => !prev.find(({ icon }) => icon === stack.icon),
+            ) ?? []),
+          ];
+        }, [])
+        .map((stack) => ({
+          ...stack,
+          slug: getSlugFromStackIcon(stack.icon),
+        }))
+        .sort((a, b) => (a.slug < b.slug ? -1 : 1)),
+    [filteredSideProjects, getSlugFromStackIcon],
   );
-
-  const isFirstRender = useRef(true);
 
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }, [page]);
 
-  // on stack change
   useEffect(() => {
-    if (isFirstRender.current) {
-      isFirstRender.current = false;
-      return;
-    }
-    const filteredSideProjects = filterSideProjects(allSideProjects);
     setPage(1);
-    setSideProjects(paginatedArray(filteredSideProjects, { page: 1, limit }));
-    setTotalAllData(filteredSideProjects.length);
-  }, [filterSideProjects, stacks, setPage]);
-
-  useEffect(() => {
-    const filteredPortfolios = filterSideProjects(allSideProjects);
-    setSideProjects(paginatedArray(filteredPortfolios, { page, limit }));
-  }, [filterSideProjects, page]);
+    setFilteredSideProjects(
+      activeStacks.length > 0
+        ? sideProjects.filter((sideProject) =>
+            activeStacks.every((activeStack) =>
+              sideProject.stacks.find(
+                (stack) => getSlugFromStackIcon(stack.icon) === activeStack,
+              ),
+            ),
+          )
+        : sideProjects,
+    );
+  }, [activeStacks, getSlugFromStackIcon, setPage]);
 
   return (
     <div className='side-project section doodle-background'>
@@ -90,34 +78,29 @@ export const SideProjectView = () => {
         />
         <div className='row justify-content-center'>
           <div className='col-auto stacks user-select-none'>
-            {stacks?.map(({ label, icon, slug, isActive }, index) => (
-              <div
-                key={index}
-                className={`stack ${isActive ? 'active' : ''}`}
-                onClick={() =>
-                  // setStacks(
-                  //   stacks.map((stack) =>
-                  //     stack.icon === icon
-                  //       ? { ...stack, isActive: !stack.isActive }
-                  //       : stack,
-                  //   ),
-                  // )
-                  setActiveStacks((value) => {
-                    return value?.includes(slug)
-                      ? value?.filter((t) => t !== slug)
-                      : [...(value ?? []), slug];
-                  })
-                }
-              >
-                <Image
-                  src={icon}
-                  alt={label}
-                  width={100}
-                  height={100}
-                  loading='eager'
-                />
-              </div>
-            ))}
+            {stacks?.map(({ label, icon, slug }, index) => {
+              return (
+                <div
+                  key={index}
+                  className={`stack ${activeStacks?.includes(slug) ? 'active' : ''}`}
+                  onClick={() =>
+                    setActiveStacks((prev) => {
+                      return prev?.includes(slug)
+                        ? prev?.filter((t) => t !== slug)
+                        : [...(prev ?? []), slug];
+                    })
+                  }
+                >
+                  <Image
+                    src={icon}
+                    alt={label}
+                    width={100}
+                    height={100}
+                    loading='eager'
+                  />
+                </div>
+              );
+            })}
           </div>
           <div
             className='col-12 text-center'
@@ -130,8 +113,8 @@ export const SideProjectView = () => {
           </div>
         </div>
         <div className='row'>
-          {sideProjects.length > 0 ? (
-            sideProjects?.map((sideProject, index) => (
+          {paginatedSideProjects.length > 0 ? (
+            paginatedSideProjects?.map((sideProject, index) => (
               <SideProjectItem key={index} {...sideProject} />
             ))
           ) : (
@@ -140,12 +123,12 @@ export const SideProjectView = () => {
             </div>
           )}
         </div>
-        {totalAllData > 0 && (
+        {filteredSideProjects.length > 0 && (
           <Pagination
             position='center'
             currentPage={page}
             setCurrentPage={setPage}
-            totalPage={Math.ceil(totalAllData / limit)}
+            totalPage={Math.ceil(filteredSideProjects.length / limit)}
           />
         )}
       </div>
