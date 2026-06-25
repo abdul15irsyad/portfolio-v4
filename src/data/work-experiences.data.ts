a93d1bf253eb594604fbb170ee96744e6239671f
Irsyad Abdul <abdulirsyad15@gmail.com>
2026-06-25 09:12:21 +0700

    fix(work-experiences): correct sorting order for first work experience

--- src/data/work-experiences.data.ts
+++ src/data/work-experiences.data.ts
@@ -231,7 +231,7 @@ export const workExperiences: WorkExperience[] = [
 ];
 
 export const firstWorkExperience = workExperiences.sort((a, b) =>
-  a.startDate < b.startDate ? -1 : 1,
+  a.startDate < b.startDate ? 1 : -1,
 )?.[0];
 
 export const yearsOfExperience = dayjs().diff(
